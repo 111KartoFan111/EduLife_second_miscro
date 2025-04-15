5ab02f2745edc0f7d9c658ce4645299d70cd7a32
111KartoFan111 <zarkynismagulov@gmail.com>
2025-04-15 18:18:57 +0500

    EduLife_Dock

--- EduLife_Dock/app/routes/__init__.py
+++ EduLife_Dock/app/routes/__init__.py
@@ -1,13 +1,9 @@
 from fastapi import APIRouter
 from .document import router as document_router
-from .auth import router as auth_router
-from .user import router as user_router
 from .registration_request import router as registration_request_router
 from .template import router as template_router
 
 router = APIRouter()
-router.include_router(auth_router)
-router.include_router(user_router)
 router.include_router(registration_request_router)
 router.include_router(template_router)
 router.include_router(document_router)
